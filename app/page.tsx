31f0a9d91921122c241af501f2ff15a99dbbdddc
Toash  Bandara <toashnb@gmail.com>
2026-05-13 12:33:20 +0530

    Add Dehiwala Zoo and Marine Drive to locations list

--- app/page.tsx
+++ app/page.tsx
@@ -174,6 +174,8 @@ export default function Home() {
                 { name: "KFC / BK / Pizza Hut", dist: "1.1 km", time: "5 MIN" },
                 { name: "Cargills / Keells", dist: "500 m", time: "2 MIN" },
                 { name: "Mountlavinia Beach", dist: "3.2 km", time: "15 MIN" },
+                { name: "Dehiwala Zoo", dist: "1.4 km", time: "6 MIN" },
+                { name: "Marine Drive", dist: "7 km", time: "14 MIN" },
                 { name: "Nearest Hospital", dist: "2.8 km", time: "12 MIN" },
                 { name: "CMB Airport", dist: "42 km", time: "55 MIN" },
                 { name: "Makumbura Multimodal Centre", dist: "16 km", time: "30 MIN" },
